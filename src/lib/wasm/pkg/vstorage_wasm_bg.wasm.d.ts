9ecb8ac504fffb2b5ed0faa118345d1e8b761313
johnsp3 <johnsp3@users.noreply.github.com>
2025-09-19 23:38:23 +0200

    Force add WASM build files for production deployment

---
+++ src/lib/wasm/pkg/vstorage_wasm_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_vstorage_free: (a: number, b: number) => void;
+export const vstorage_new: (a: number) => number;
+export const vstorage_write: (a: number, b: number, c: number, d: number) => [number, number];
+export const vstorage_read: (a: number, b: number, c: number) => [number, number, number, number];
+export const vstorage_get_capacity: (a: number) => number;
+export const vstorage_get_used: (a: number) => number;
+export const vstorage_clear: (a: number) => void;
+export const vstorage_optimize: (a: number) => number;
+export const main: () => void;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
